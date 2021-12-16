684d3f5dac559575f4c0c967ece22085800290ae
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-12-15 16:49:49 -0800

    made a home page and linked it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import BookList from './views/Books/BookList';
+import Home from './views/Home/Home';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/books">
+            <BookList />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </main>
   );
 }
---
+++ src/views/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Library Catalog</h1>
+      <h2>Welcome to Dangalfs Library of Computer Science!</h2>
+      <p>
+        Check out the amazing selection of books in our <Link to={`/books/`}>catalog</Link>.
+      </p>
+      <Link to={`/books/`}></Link>
+    </div>
+  );
+}
