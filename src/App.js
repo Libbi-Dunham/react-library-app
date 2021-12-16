ed1cc2fabe74dfae82e2c383d1361d5aef9faf0d
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-12-15 17:32:57 -0800

    gave books links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import BookList from './views/Books/BookList';
 import Home from './views/Home/Home';
+import Book from './views/Books/BookDetail';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
@@ -15,6 +16,7 @@ function App() {
           <Route path="/books">
             <BookList />
           </Route>
+          <Route path="/books/id:" component={Book} />
         </Switch>
       </BrowserRouter>
     </main>
--- src/components/book/Book.js
+++ src/components/book/Book.js
@@ -1,27 +1,38 @@
 import Author from '../author/Author';
 import './Book.css';
+import { Link } from 'react-router-dom';
 
 function Book({
-  book: { title, total_pages: totalPages, rating, isbn, published_date: publishedDate, authors },
+  book: {
+    title,
+    total_pages: totalPages,
+    rating,
+    isbn,
+    published_date: publishedDate,
+    authors,
+    books_id,
+  },
   showDetail = false,
 }) {
   return (
-    <article className="book">
-      <h3>{title}</h3>
-      <address>
-        {authors.map((author) => (
-          <Author key={author.author_id} author={author} />
-        ))}
-      </address>
-      {showDetail && (
-        <>
-          <p>Rating: {rating}</p>
-          <p>ISBN: {isbn}</p>
-          <p>Published {publishedDate}</p>
-          <p>{totalPages} pages</p>
-        </>
-      )}
-    </article>
+    <Link to={`/books/${books_id}`}>
+      <article className="book">
+        <h3>{title}</h3>
+        <address>
+          {authors.map((author) => (
+            <Author key={author.author_id} author={author} />
+          ))}
+        </address>
+        {showDetail && (
+          <>
+            <p>Rating: {rating}</p>
+            <p>ISBN: {isbn}</p>
+            <p>Published {publishedDate}</p>
+            <p>{totalPages} pages</p>
+          </>
+        )}
+      </article>
+    </Link>
   );
 }
 
